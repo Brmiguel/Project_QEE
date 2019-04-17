6f52a1570f0a15794ef67adeeb2a16782cea77f6
Brmiguel <bruno12.scp@gmail.com>
2019-04-17 20:54:25 +0100

    Save Data in file .txt

--- Menu/bluetooth.cpp
+++ Menu/bluetooth.cpp
@@ -2,6 +2,65 @@
 #include <QtBluetooth>
 
 
+bool Bluetooth::readFile()
+{
+    QFile file("Dados.txt");
+    if(!file.exists())
+    {
+        qDebug() << file.fileName() << " does not exists";
+    }
+
+    if(file.open(QIODevice::ReadOnly | QIODevice::WriteOnly | QIODevice::Text))
+    {
+        QTextStream textStream(&file);
+
+        qDebug() << " ----- Reading from file ------" ;
+        textStream.seek(0);
+        while (!textStream.atEnd()) {
+
+            qDebug() << textStream.readLine() ; //textStream.readAll()
+
+
+        }
+
+        file.close();
+        return true;
+    }
+
+    else {
+        qDebug() << " Could not open the file" ;
+        return false;
+    }
+}
+
+bool Bluetooth::writeFile()
+{
+    QFile file("Dados.txt");
+    if(!file.exists())
+    {
+        qDebug() << file.fileName() << " does not exists";
+    }
+
+    if(file.open(QIODevice::ReadOnly | QIODevice::WriteOnly | QIODevice::Text))
+    {
+        QTextStream textStream(&file);
+
+        qDebug() << " ----- Writing to file ------" ;
+
+        textStream << "The first Line\n";
+        textStream << "The second Line\n";
+        textStream << "Note that the >> perator does not appends a new line automatically at the end\n";
+
+        file.close();
+        return true;
+    }
+
+    else {
+        qDebug() << " Could not open the file" ;
+        return false;
+    }
+}
+
 Bluetooth::Bluetooth(QObject *parent)
     : QObject(parent)
 {
@@ -27,6 +86,10 @@ Bluetooth::Bluetooth(QObject *parent)
 
         agent->start();
     }
+
+    QFile file("Dados.txt");
+
+
 
     p_P=600;
     p_Q=300;
